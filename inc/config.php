b03a02889a5f959b25084801ea573aab47b76cba
aghos <jonas@hyp3.be>
2016-12-05 18:04:43 +0100

    Update config.php
    empty config

--- inc/config.php
+++ inc/config.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-	'servername' => '172.16.200.115',
-	'username' => 'dbu_fosdem',
-	'password' => 'temptest',
-	'dbname' => 'db_fosdem',
+	'servername' => '',
+	'username' => '',
+	'password' => '',
+	'dbname' => '',
 );
 
