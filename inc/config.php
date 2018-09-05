09f36f300dbaf6e474f082fae1427a6e82f7da5c
Ray O'Donnell <ray@teladesign.ie>
2018-09-05 17:07:40 +0100

    Initial port of the database creation script. Added a .gitignore also for my own environment.

--- inc/config.php
+++ inc/config.php
@@ -1,9 +1,10 @@
 <?php
 
 return array(
-	'servername' => '127.0.0.1',
-	'username' => 'placeholder',
-	'password' => 'placeholder',
-	'dbname' => 'db_fosdem',
+//	'servername' => '127.0.0.1',
+//	'username' => 'placeholder',
+//	'password' => 'placeholder',
+//	'dbname' => 'db_fosdem',
+    'conn_str' => 'host=localhost port=5435 dbname=tshirts username=fosdem_temp password=fosdem_temp',
 );
 
