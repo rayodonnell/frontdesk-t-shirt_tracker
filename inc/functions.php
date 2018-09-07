1afe8a181e38314549bc8fba6e89d32c980aa007
Ray O'Donnell <ray@teladesign.ie>
2018-09-07 21:23:51 +0100

    Ported the remaining functions in functions.php.

--- inc/functions.php
+++ inc/functions.php
@@ -3,6 +3,7 @@
 $config = include('config.php');
 session_start();
 
+
 function bind($bind, $id)
 {
     global $config;
@@ -47,9 +48,9 @@ function countFos($fid, $group, $title, $room, $operator, $text, $print, $print2
                 $total = $row['fcount'] - 1;
             }
         }
+        pg_free_result($rs);
     }
 
-    pg_free_result($rs);
 
     $sql = 'insert into fosdem (fid, fgroup, fcount) values ($1, $2, $3)'
         . ' on conflict (fid) do update set fcount = excluded.fcount';
@@ -79,7 +80,6 @@ function getCount($fid)
 {
     global $config;
 
-
     $conn = pg_connect($config['conn_str']);
     if (pg_connection_status($conn) != PGSQL_CONNECTION_OK) {
         die('Connection failed: ' . pg_last_error($conn));
@@ -93,13 +93,12 @@ function getCount($fid)
     }
     else {
         $total = 0;
-
         while ($row = pg_fetch_assoc($rs)) {
             $total = $row['fcount'];
         }
+        pg_free_result($rs);
     }
 
-    pg_free_result($rs);
     pg_close($conn);
 
     return $total;
@@ -109,80 +108,91 @@ function getCount($fid)
 function getCountTotal($fgroup)
 {
     global $config;
-    $conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["dbname"]);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+
+    $conn = pg_connect($config['conn_str']);
+    if (pg_connection_status($conn) != PGSQL_CONNECTION_OK) {
+        die('Connection failed: ' . pg_last_error($conn));
     }
-    $sql = "SELECT fcount FROM fosdem where fgroup=?";
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param("i", $fgroup);
-        $stmt->execute();
+
+    $sql = 'select fcount from fosdem where fgroup = $1';
+    $rs = pg_query_params($conn, $sql, array($fgroup));
+
+    if ($rs === false) {
+        die('Error: ' . pg_last_error($conn));
+    }
+    else {
         $total = 0;
-        $stmt->bind_result($fcount);
-        while ($stmt->fetch()) {
-            $total += $fcount;
+        while ($row = pg_fetch_assoc($rs)) {
+            $total += $row['fcount'];
         }
     }
-    $stmt->close();
-    $conn->close();
+
+    pg_close($conn);
+
     return $total;
 }
+
 
 function printer($title, $room, $print, $print2, $id)
 {
     global $config;
-    $conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["dbname"]);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+
+    $conn = pg_connect($config['conn_str']);
+    if (pg_connection_status($conn) != PGSQL_CONNECTION_OK) {
+        die('Connection failed: ' . pg_last_error($conn));
     }
-    $sql = "INSERT INTO fosdem_print (printid,printtype,printvalue,status,print,print2,eventid) VALUES (?,'donation',?,0,?,?,?);";
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param('isssi', $room, $title, $print, $print2, $id);
-        $stmt->execute();
-    }
-    $stmt->close();
-    $conn->close();
+
+    $sql = 'insert into fosdem_print (printid, printtype, printvalue, status, print, print2, eventid)'
+        . " values ($1, 'donation', $2, 0, $3, $4, $5)";
+    pg_query_params($conn, $sql, array($room, $title, $print, $print2, $id));
+
+    pg_close($conn);
 }
+
 
 function logger($fid, $group, $action, $room)
 {
     global $config;
-    $conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["dbname"]);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+
+    $conn = pg_connect($config['conn_str']);
+    if (pg_connection_status($conn) != PGSQL_CONNECTION_OK) {
+        die('Connection failed: ' . pg_last_error($conn));
     }
+
     $time = time();
-    $sql = "INSERT INTO fosdem_logger (fid,fgroup,fdate,faction,room) VALUES (?,?,?,?,?);";
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param("iisss", $fid, $group, $time, $action, $room);
-        $stmt->execute();
-    }
-    $stmt->close();
-    $conn->close();
+
+    $sql = 'insert into fosdem_logger (fid, fgroup, fdate, faction, room)'
+        . ' values ($1, $2, $3, $4, $5)';
+    pg_query_params($conn, $sql, array($fid, $group, $time, $action, $room));
+
+    pg_close($conn);
 }
+
 
 function getVisible($gid)
 {
     $total = 1;
-    if (isset($_SESSION["G" . $gid])) {
-        $total = $_SESSION["G" . $gid];
+    if (isset($_SESSION['G' . $gid])) {
+        $total = $_SESSION['G' . $gid];
     }
     if ($total == 0) {
-        $total = "hidden";
+        $total = 'hidden';
     }
     else {
-        $total = "";
+        $total = '';
     }
     return $total;
 }
 
+
 function hideGroup($group, $state)
 {
-    $_SESSION["G" . $group] = $state;
+    $_SESSION['G' . $group] = $state;
 }
+
 
 function sendError()
 {
-    echo "error incorrect format";
+    echo 'error incorrect format';
     exit();
 }
