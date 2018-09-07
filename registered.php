7a35f0c2fe263335f9de10725635d28541f8a090
Ray O'Donnell <ray@teladesign.ie>
2018-09-07 21:59:30 +0100

    Removed a no-longer-needed function.

--- registered.php
+++ registered.php
@@ -110,21 +110,3 @@ if ($login->isUserLoggedIn() == true) {
 else {
     echo "Access denied.";
 }
-
-function stmt_bind_assoc(&$stmt, &$out)
-{
-    $data = mysqli_stmt_result_metadata($stmt);
-    $fields = array();
-    $out = array();
-
-    $fields[0] = $stmt;
-    $count = 1;
-
-    while ($field = mysqli_fetch_field($data)) {
-        $fields[$count] = &$out[$field->name];
-        $count++;
-    }
-    call_user_func_array(mysqli_stmt_bind_result, $fields);
-}
-
-?>
